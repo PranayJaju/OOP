25b83c7fbddec4417dd4ce23cf0e095ddd16350f
PranayJaju <98613449+PranayJaju@users.noreply.github.com>
2022-09-13 20:10:35 +0530

    Update StudentRecord.java

--- Lab5/StudentRecord.java
+++ Lab5/StudentRecord.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 class Student
 {
 	int RegNo;
@@ -31,15 +29,16 @@ class Student
 
     public void display()
     {
-        System.out.println("Name: "+fname);
+        System.out.printf("%-25s%-20s\n","Name:",fname);
         if(doj.get(Calendar.YEAR)<2010)
-            System.out.println("Registration Number: 0"+RegNo);
+            System.out.printf("%-25s0%-10d\n","Registration Number:", RegNo);
         else 
-        	System.out.println("Registration Number: "+RegNo);
-        System.out.println("Semester: "+sem);
-        System.out.print("Date of Joining: ");
+        	System.out.printf("%-25s%-10d\n","Registration Number:", RegNo);
+        System.out.printf("%-25s%-10d\n", "Semester:", sem);
+        System.out.printf("%-25s", "Date of Joining:");
         System.out.println(doj.get(Calendar.DATE)+"/"+doj.get(Calendar.MONTH)+"/"+doj.get(Calendar.YEAR));
-        System.out.println("GPA: "+gpa+"\t CGPA: "+cgpa);
+        System.out.printf("%-25s%-10.2f\n", "GPA:", gpa);
+        System.out.printf("%-25s%-10.2f\n", "CGPA:", cgpa);
     }
 
     public int calRegNo(int stuNo, int year)
@@ -114,6 +113,23 @@ class Student
     		System.out.println();
     	}
     }
+
+    public void NameChange(Student s[])
+    {
+        for(int i=0; i<s.length; i++)
+        {
+            String newName="";
+            String individual_name[]=s[i].fname.split(" ");
+            for(int j=0; j<individual_name.length; j++)
+            {
+                if(j==individual_name.length-1)
+                    newName=newName+individual_name[j];
+                else
+                    newName=newName+individual_name[j].charAt(0)+". ";
+            }
+            s[i].fname=newName;
+        }
+    }
 }
 
 class StudentRecord
@@ -133,10 +149,10 @@ class StudentRecord
 	    	System.out.println("Enter Name.");
 	        sc.nextLine();
 	        na=sc.nextLine();
-	        System.out.println("Enter Date of Joining.");
-	        year=sc.nextInt();
-	        month=sc.nextInt();
+	        System.out.println("Enter Date of Joining.(dd/mm/yyyy)");
 	        day=sc.nextInt();
+	        month=sc.nextInt();
+            year=sc.nextInt();
 	        System.out.println("Enter GPA and CGPA.");
 	        gpa=sc.nextFloat();
 	        cgpa=sc.nextFloat();
@@ -149,7 +165,7 @@ class StudentRecord
         System.out.println();
 
         System.out.println("*******************************");
-	    System.out.println("Sorted according to Semester and CGPA.");
+	    System.out.println("Sorted according to Semester and CGPA.\n");
 	    Student dummy=new Student();
 	    dummy.Sem_CGPA_Sort(s);
 	    for(int i=0; i<n; i++)
@@ -159,7 +175,7 @@ class StudentRecord
 	    }
         
         System.out.println("*******************************");
-        System.out.println("Sorted According to Name.");
+        System.out.println("Sorted According to Name.\n");
         dummy.Name_Sort(s);
         for(int i=0; i<n; i++)
         {
@@ -176,5 +192,14 @@ class StudentRecord
         System.out.println("Enter Substring to be checked with.");
         String sub=sc.next();
         dummy.Contains_Sub(s, sub);
+        
+        System.out.println("*******************************");
+        System.out.println("Name Changed to Initials and Family Name.\n");
+        dummy.NameChange(s);
+        for(int i=0; i<n; i++)
+        {
+        	s[i].display();
+        	System.out.println();
+        }
 	}
 }
